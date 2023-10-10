cde55cea30da96037705829ca3d9405c7cf7c719
Ivan Sendin <ivansendin@yahoo.com>
2023-10-09 23:05:32 -0300

    Initial commit

---
+++ Contracts/tinyDeX.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.6.0;
+
+import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract tinyDeX {
+
+  address owner;
+
+  struct PoolType {
+    address poolOwner;
+    ERC20 x;
+    ERC20 y;
+    }
+
+  mapping (string=>PoolType) pools;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function createPool(string memory name, ERC20 x, ERC20 y) public {
+    require (pools[name].poolOwner == address(0),"This pool already exists!");
+    require (x.allowance(msg.sender,address(this))>0,"Show me the token!");
+    require (y.allowance(msg.sender,address(this))>0,"Show me the token!");
+
+    x.transferFrom(msg.sender,address(this),x.allowance(msg.sender,address(this)));
+    y.transferFrom(msg.sender,address(this),y.allowance(msg.sender,address(this)));
+
+    pools[name].poolOwner = msg.sender;
+    pools[name].x = x;
+    pools[name].y = y;
+  }
+
+  // buy y token using x token
+  function buy(string memory name) public {
+    require (pools[name].poolOwner != address(0),"This pool doesnt exist!");
+    require (pools[name].x.allowance(msg.sender,address(this))>0,"Show me the token!");
+    uint256 xammount = pools[name].x.allowance(msg.sender,address(this));
+    uint256 xbalance = pools[name].x.balanceOf(address(this));
+    uint256 ybalance = pools[name].y.balanceOf(address(this));
+
+    uint256 yammount = (ybalance*xammount)/(xbalance+xammount);
+
+    pools[name].x.transferFrom(msg.sender,address(this),xammount);
+    pools[name].y.transfer(msg.sender,yammount);
+
+  }
+
+function sell(string memory name) public {
+    require (pools[name].poolOwner != address(0),"This pool doesnt exist!");
+    require (pools[name].y.allowance(msg.sender,address(this))>0,"Show me the token!");
+    uint256 yammount = pools[name].y.allowance(msg.sender,address(this));
+    uint256 ybalance = pools[name].y.balanceOf(address(this));
+    uint256 xbalance = pools[name].x.balanceOf(address(this));
+
+    uint256 xammount = (xbalance*yammount)/(ybalance+yammount);
+
+    pools[name].y.transferFrom(msg.sender,address(this),yammount);
+    pools[name].x.transfer(msg.sender,xammount);
+
+  }
+
+
+}
